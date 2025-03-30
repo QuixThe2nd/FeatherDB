4e795338f6ff4c3008436cd2d58051e8390ae00c
Parsa Yazdani <parsa@yazdani.au>
2025-03-30 20:48:25 +1100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,22 +1,14 @@
 import { Database as BunDatabase, type SQLQueryBindings } from "bun:sqlite";
-import type { Database as BrowserDatabase, SqlValue } from 'sql.js';
+import { type Database as BrowserDatabase, type SqlValue } from 'sql.js';
 
 type SQLTypes = SqlValue & SQLQueryBindings
 export type ValidValuesOnly<T> = { [K in keyof T]: T[K] extends string | number | boolean | bigint ? T[K] : never }
+export interface DefinitionOpt { type: 'INTEGER' | 'TEXT' | 'BOOLEAN', nullable?: boolean, primaryKey?: boolean }
 export type Definition<T> = { [key in keyof T]: DefinitionOpt }
 export type WhereObject<T> = { value: T[] | T, type: '=' | '>' | '<' | '>=' | '<=' | '!=' }
 export type Where<T> = { column: keyof Partial<T> & string, opt: WhereObject<T[keyof T]> | undefined }
 export type OrderBy<T> = { [key in keyof Partial<T>]: 'ASC' | 'DESC'; }
-export interface DefinitionOpt {
-  type: 'INTEGER' | 'TEXT' | 'BOOLEAN',
-  nullable?: boolean,
-  primaryKey?: boolean
-}
-export type GetOptions<T> = {
-  where?: Where<T>[] | undefined;
-  limit?: number | undefined;
-  orderBy?: OrderBy<T> | undefined;
-}
+export type GetOptions<T> = { where?: Where<T>[] | undefined; limit?: number | undefined; orderBy?: OrderBy<T> | undefined }
 
 export const eq = <T>(value: T & SQLTypes): WhereObject<T> => { return { type: "=", value } }
 export const gt = <T>(value: T & SQLTypes): WhereObject<T> => { return { type: ">", value } }
@@ -48,20 +40,22 @@ const buildOpts = <T>(opts?: GetOptions<T>) => {
   return { builtQuery, values }
 }
 
-export class Table<T extends object, R extends T> {
-  name: string
-  definition: Definition<T>
+export interface TableSchema<T, RowClass> {
+  readonly name: string
+  readonly definition: Definition<T>
+  readonly child: new (args: T) => RowClass
+}
+
+export class Table<T extends object, RowClass> {
   private db: BunDatabase | BrowserDatabase
-  private child: new (args: T) => T & R
-  constructor(db: BunDatabase | BrowserDatabase, name: string, definition: Definition<T>, child: new (args: T) => T & R) {
-    this.name = name
-    this.definition = definition
+  private schema: TableSchema<T, RowClass>
+  constructor(db: BunDatabase | BrowserDatabase, schema: TableSchema<T, RowClass>) {
     this.db = db
-    this.child = child
+    this.schema = schema
   }
   create = (): void => {
-    const columnDefs = (Object.entries(this.definition) as [string, DefinitionOpt][]).map(([col, opts]) => `${col} ${opts.type}${opts.primaryKey ? ' PRIMARY KEY' : ''}${!opts.nullable && !opts.primaryKey ? ' NOT NULL' : ''}`).join(', ');
-    const query = `CREATE TABLE IF NOT EXISTS ${this.name} (${columnDefs})`
+    const columnDefs = (Object.entries(this.schema.definition) as [string, DefinitionOpt][]).map(([col, opts]) => `${col} ${opts.type}${opts.primaryKey ? ' PRIMARY KEY' : ''}${!opts.nullable && !opts.primaryKey ? ' NOT NULL' : ''}`).join(', ');
+    const query = `CREATE TABLE IF NOT EXISTS \`${this.schema.name}\` (${columnDefs})`
     console.log(query)
     this.db.run(query);
   }
@@ -70,29 +64,29 @@ export class Table<T extends object, R extends T> {
     const values: Array<T[keyof T]> = columns.map((col) => row[col])
     const placeholders = columns.map(_ => '?').join(', ')
 
-    const query = `INSERT INTO ${this.name} (${columns.join(', ')}) VALUES (${placeholders})`
+    const query = `INSERT INTO ${this.schema.name} (${columns.join(', ')}) VALUES (${placeholders})`
     console.log(query)
     if ('create_function' in this.db) this.db.prepare(query).getAsObject(values as Array<T[keyof T] & SQLTypes>)
     else this.db.query(query).run(...values as Array<T[keyof T] & SQLTypes>)
   }
-  get = (opts?: GetOptions<T>): R[] => {
+  get = (opts?: GetOptions<T>): RowClass[] => {
     const { builtQuery, values } = buildOpts(opts)
-    const query = `SELECT * FROM ${this.name}${builtQuery}`
+    const query = `SELECT * FROM \`${this.schema.name}\`${builtQuery}`
 
     console.log(query)
     if ('create_function' in this.db) {
       const stmt = this.db.prepare(query)
       stmt.bind(values)
-      const rows: R[] = []
+      const rows: RowClass[] = []
       while (stmt.step()) {
-        rows.push(new this.child(stmt.getAsObject() as T))
+        rows.push(new this.schema.child(stmt.getAsObject() as T))
       }
       return rows
-    } else return this.db.query(query).as(this.child).all(...values) as R[]
+    } else return this.db.query(query).as(this.schema.child).all(...values) as RowClass[]
   }
   delete = (opts?: GetOptions<T>): void => {
     const { builtQuery, values } = buildOpts(opts)
-    const query = `DELETE FROM ${this.name}${builtQuery}`
+    const query = `DELETE FROM ${this.schema.name}${builtQuery}`
 
     console.log(query)
     if ('create_function' in this.db) this.db.run(query, values)
@@ -100,7 +94,7 @@ export class Table<T extends object, R extends T> {
   }
   count = (where?: Where<T>[]): number => {
     const { builtQuery, values } = buildOpts({ where })
-    const query = `SELECT COUNT(*) as count FROM ${this.name}${builtQuery}`
+    const query = `SELECT COUNT(*) as count FROM ${this.schema.name}${builtQuery}`
 
     if ('create_function' in this.db) return (this.db.prepare(query).getAsObject(values) as { count: number }).count
     else return (this.db.query(query).all(...values)[0] as { count: number }).count ?? 0
@@ -108,11 +102,11 @@ export class Table<T extends object, R extends T> {
   update = (partialRow: Partial<T>, opts?: GetOptions<T>): void => {
     if (!partialRow || Object.keys(partialRow).length === 0) throw new Error('Update requires at least one field to update');
 
-    const updateFields = Object.entries(partialRow).filter(([key]) => key in this.definition).map(([key]) => `${key} = ?`).join(', ');
+    const updateFields = Object.entries(partialRow).filter(([key]) => key in this.schema.definition).map(([key]) => `${key} = ?`).join(', ');
     const { builtQuery, values } = buildOpts(opts)
-    const query = `UPDATE ${this.name} SET ${updateFields} ${builtQuery}`
+    const query = `UPDATE ${this.schema.name} SET ${updateFields} ${builtQuery}`
 
-    const updateValues: (T[keyof T] & SQLTypes)[] = (Object.entries(partialRow) as [keyof T, (T[keyof T] & SQLTypes)][]).filter(([key]) => key in this.definition).map(([_, value]) => value)
+    const updateValues: (T[keyof T] & SQLTypes)[] = (Object.entries(partialRow) as [keyof T, (T[keyof T] & SQLTypes)][]).filter(([key]) => key in this.schema.definition).map(([_, value]) => value)
     const params: (T[keyof T] & SQLTypes)[] = [...updateValues, ...values];
 
     console.log(query)
